19751f35e6e697c4391a3b48170a22e82821a8b7
NicholasFoti <nickfoti@hotmail.com>
2024-12-05 02:04:25 +0800

    routes

--- backend/app.js
+++ backend/app.js
@@ -25,6 +25,10 @@ app.use(express.static(path.join(__dirname, '../frontend/build')));
 app.use('/api/auth', authRoutes);
 app.use('/api/game', gameRoutes);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+});
+
 // Error handling middleware
 app.use((err, req, res, next) => {
     console.error(err.stack);
