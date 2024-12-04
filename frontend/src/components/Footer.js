1909ee52783897c77d46892e5a9f6d3986630530
NicholasFoti <nickfoti@hotmail.com>
2024-12-05 03:47:03 +0800

    Footer Changes

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Footer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSackDollar } from '@fortawesome/free-solid-svg-icons';
 
 const Footer = () => {
   return (
     <footer className="casino-footer">
-      <p>&copy; 2023 Fake Casino. All rights reserved.</p>
+      <p>Made by <a href="https://github.com/NicholasFoti">Nicholas Foti</a><FontAwesomeIcon className="casino-footer-icon" icon={faSackDollar} /></p>
     </footer>
   );
 };
